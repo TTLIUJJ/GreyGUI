1de91ab9fa7b1fd11c74814ce37b479524f5da4c
ackerman <TTLIUJJ@gmail.com>
2018-12-19 21:11:05 +0800

    readme.md

---
+++ gui/GreyGUI.java
@@ -0,0 +1,63 @@
+package gui;
+
+import test.SwingConsole;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GreyGUI extends JFrame {
+
+    public GreyGUI() {
+
+    }
+
+    public static void main(String []args) {
+        SwingConsole.run(new GreyGUI(), 800, 600);
+    }
+}
+
+class GBC extends GridBagConstraints {
+    public GBC(int gridx, int gridy) {
+        this.gridx = gridx;
+        this.gridy = gridy;
+    }
+
+    public GBC(int gridx, int grdiy, int gridwidth, int gridheight) {
+        this.gridx = gridx;
+        this.gridy = grdiy;
+        this.gridwidth  = gridwidth;
+        this.gridheight = gridheight;
+    }
+
+    public GBC setAnchor(int anchor) {
+        this.anchor = anchor;
+        return this;
+    }
+
+    public GBC setFill(int fill) {
+        this.fill = fill;
+        return this;
+    }
+
+    public GBC setWeight(int weightx, int weighty) {
+        this.weightx = weightx;
+        this.weighty = weighty;
+        return this;
+    }
+
+    public GBC setInsets(int distance) {
+        this.insets = new Insets(distance, distance, distance, distance);
+        return this;
+    }
+
+    public GBC setINsets(int top, int left, int bottom, int right) {
+        this.insets = new Insets(top, left, bottom, right);
+        return this;
+    }
+
+    public GBC setIpad(int ipadx, int ipady) {
+        this.ipadx = ipadx;
+        this.ipady = ipady;
+        return this;
+    }
+}
